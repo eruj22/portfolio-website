ca58739a3f8eca1c2d366d85838e7c63f139f601
eruj22 <prci.prnaver2@gmail.com>
2021-05-10 10:30:30 +0200

    final touches and making sure that site looks good across all popular browsers

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,35 +1,62 @@
-// Navigation
-const menuBtn = document.getElementById('toggle-btn');
-const nav = document.getElementById('nav');
+// Form validation
+const form = document.getElementById('form');
+const userName = document.getElementById('name');
+const email = document.getElementById('email');
+const comment = document.getElementById('comment');
 
-let menuOpen = false;
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkInputs();
+    // refresh page and sends form when every input is ok
+    window.location = this.href;
+})
 
-menuBtn.addEventListener('click', toggleMenu);
+function checkInputs() {
+    // get the values from inputs
+    const userNameValue = userName.value.trim();
+    const emailValue = email.value.trim();
+    const commentValue = comment.value.trim();
 
-function toggleMenu() {
-    if (!menuOpen) {
-        menuBtn.classList.add('open');
-        nav.classList.add('open');
-        menuOpen = true;
+    // check userName
+    if (userNameValue === '' || userNameValue === null) {
+        setError(userName, 'Name can\'t be blank!')
     } else {
-        menuBtn.classList.remove('open');
-        nav.classList.remove('open');
-        menuOpen = false;
+        setSuccess(userName);
+    }
+
+    // check email
+    if (emailValue === '') {
+        setError(email, 'Email can\'t be blank');
+    } else if (!isEmail) {
+        setError(email, 'Email is not valid');
+    } else {
+        setSuccess(email);
+    }
+
+    // check comment
+    if (commentValue === '') {
+        setError(comment, 'Comment can\'t be blank');
+    } else {
+        setSuccess(comment);
     }
 }
 
-// scroll magic for projects page
-// function splitScroll() {
-//     // handles all the scroll on our page
-//     const controller = new scrollMagic.Controller();
+function setSuccess(input) {
+    const formControl = input.parentElement;
 
-//     new ScrollMagic.Scene({
-//         duration: 500,
-//         triggerElement: '.projects__title',
-//         triggerHook: 0
-//     })
-//     .setPin('.projects__title')
-//     .addTo(controller);
-// }
+    formControl.className = 'form-control success';
+}
 
-// splitScroll();
+function setError(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+
+    formControl.className = 'form-control error';
+}
+
+function isEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+};
