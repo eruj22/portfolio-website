2380d735cfd0532447abeaa328d3b437b9a436f5
eruj22 <prci.prnaver2@gmail.com>
2021-05-23 18:35:50 +0200

    added better navigation

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,67 @@
+// fixed header
+const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset;
+    const topLink = document.querySelector('.top-link');
+
+    if (scrollHeight > headerHeight) {
+        header.classList.add('header-fixed');
+    } else {
+        header.classList.remove('header-fixed');
+    }
+    
+    if (scrollHeight > 500) {
+        topLink.classList.add('show-link');
+    } else {
+        topLink.classList.remove('show-link');
+    }
+});
+
+// hamburger menu
+const nav = document.querySelector('.nav');
+const toggle = document.querySelector('.nav__toggle');
+let navOpen = false;
+
+toggle.addEventListener('click', () => {
+    if (!navOpen) {
+        nav.classList.add('open');
+        navOpen = true;
+    } else {
+        nav.classList.remove('open');
+        navOpen = false;
+    }
+});
+
+// smooth scroll
+const scrollLinks = document.querySelectorAll('.smooth-scroll');
+const linksContainer = document.querySelector('.nav__menu');
+
+scrollLinks.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        // navigate to specific spot
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+        // calculate the heights
+        const fixedNav = nav.classList.contains('header-fixed');
+        let position = element.offsetTop - headerHeight;
+
+        if (!fixedNav) {
+            position = position - headerHeight;
+        }
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+            behavior: 'smooth'
+        })
+        nav.classList.remove('open');
+        navOpen = false;
+    })
+});
+
 // Form validation
 const form = document.getElementById('form');
 const userName = document.getElementById('name');
@@ -6,26 +70,25 @@ const comment = document.getElementById('comment');
 
 let i = 0;
 
-form.addEventListener("submit", handleSubmit)
+form.addEventListener("submit", handleSubmit);
 
-async function handleSubmit(event) {
-    event.preventDefault();
-    if (i < 3) {
-        checkInputs();
-    } else {
+async function handleSubmit(e) {
+    e.preventDefault();
+    checkInputs();
+    if (i === 3) {
         let status = document.getElementById("my-form-status");
-        let data = new FormData(event.target);
-        fetch(event.target.action, {
+        let data = new FormData(e.target);
+        fetch(e.target.action, {
             method: form.method,
             body: data,
             headers: {
-            'Accept': 'application/json'
-        }
+                'Accept': 'application/json'
+            }
         }).then(response => {
-        status.innerHTML = "Thanks for your submission!";
-        form.reset()
+            status.innerHTML = "Thanks for your submission!";
+            form.reset()
         }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
+            status.innerHTML = "Oops! There was a problem submitting your form"
         });
     }
 }
@@ -66,7 +129,6 @@ function checkInputs() {
 
 function setSuccess(input) {
     const formControl = input.parentElement;
-
     formControl.className = 'form-control success';
 }
 
